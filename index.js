49d3d03f41b1769c886abd3cdc7b19863f806cbe
Andreas Straninger <andreas.straninger@accenture.com>
2017-02-23 14:24:21 +0100

    added detail controller

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ angular.module('mySwapper').config (function($routeProvider){
   $routeProvider.when('/', {
     controller: "startController",
     templateUrl: "controller/startController.html"
+  })
+  .when('/detail', {
+	  controller: "detailController",
+	  templateUrl: "detailTemplate.html"
   });
 });
