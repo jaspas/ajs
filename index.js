38d36fbf443d7cab3b04986e7e6d0d352c9db567
Canol <kasim.canol@accenture.com>
2017-02-23 15:17:00 +0100

    changes in tauschangebote

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ angular.module('mySwapper').config (function($routeProvider){
   $routeProvider.when('/', {
     controller: "startController",
     templateUrl: "controller/startController.html"
+  }).when('/tauschangebote', {
+    controller: "tauschangebotController",
+    templateUrl: "controller/tauschangebot/tauschangebot.html"
   });
 });
