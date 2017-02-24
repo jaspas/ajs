be0ee2d7d972ea11e2cc62cd70aec531cbd4b21c
Parashin <vladimir.parashin@accenture.com>
2017-02-24 11:40:21 +0100

    detailpage for given id

--- controller/detailpage.js
+++ controller/detailpage.js
@@ -19,16 +19,24 @@ angular.module('mySwapper')
   );
 })*/
 
-.controller('detailpage', function($scope) {
-//   var self = this;
-//   self.id = $routeParams.id;
-  $scope.trade = {id: 111,
-      	        owner: "Jack",
-                location: "San Francisco",
-                time: "10:00" ,
-                name: "Regenschirm",
-                condition: "new" ,
-                description: "You'll never get wet! Even if it rains hard.",
-                tradeWish: "Ferrari"};
+.controller('detailpage', function($scope,$resource,$routeParams) {
+var Trade = $resource('https://intense-mesa-72431.herokuapp.com/swaps/:id', {id: '@_id'});
+
+  // var currentTrade = Trade.get({id: $routeParams.id}, function(){
+  //
+  //   $scope.trade = currentTrade;
+  // });
+  var currentTrade = Trade.get({ id: $routeParams.id }, function() {
+   $scope.trade = currentTrade;
+ }); // get() returns a single entry
+
+  // $scope.trade = {id: 111,
+  //     	        owner: "Jack",
+  //               location: "San Francisco",
+  //               time: "10:00" ,
+  //               name: "Regenschirm",
+  //               condition: "new" ,
+  //               description: "You'll never get wet! Even if it rains hard.",
+  //               tradeWish: "Ferrari"};
 
 });
